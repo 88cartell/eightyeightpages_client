cadc5f6e2d409b258ef0f589a7b5a5dabec47948
Carl Woodward <cjwoodward@gmail.com>
2012-01-04 14:43:19 +1100

    Trialing a fix for encoding issue

--- lib/eightyeightpages_client.rb
+++ lib/eightyeightpages_client.rb
@@ -12,9 +12,26 @@ module EightyeightpagesClient
       self.class.base_uri "#{account_name}.88pages.com"
     end
 
+    def unescape(hash)
+      if hash.is_a?(Array)
+        hash.each do |value|
+          unescape hash
+        end
+      elsif hash.is_a?(Hash)
+        hash.keys.each do |key|
+          if hash[key].is_a?(String)
+            hash[key] = CGI::unescape(hash[key])
+          elsif hash[key].is_a?(Hash) or hash[key].is_a?(Array)
+            unescape hash[key]
+          end
+        end
+      end
+      hash
+    end
+
     def method_missing(form, *args)
       begin
-        self.class.get("/api/forms/#{form.to_s.gsub('_', '-')}/entries.json?referer=#{referer}").parsed_response
+        unescape self.class.get("/api/forms/#{form.to_s.gsub('_', '-')}/entries.json?referer=#{referer}").parsed_response
       rescue
         rescue super(field, *args)
       end
