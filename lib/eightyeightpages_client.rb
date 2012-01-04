29e2010cd71b68224c17acd70085d6947f6309ce
Carl Woodward <cjwoodward@gmail.com>
2012-01-04 14:54:57 +1100

    Trying map

--- lib/eightyeightpages_client.rb
+++ lib/eightyeightpages_client.rb
@@ -1,5 +1,6 @@
 require 'eightyeightpages_client/version'
 require 'httparty'
+require 'map'
 
 module EightyeightpagesClient
   class Query
@@ -12,26 +13,9 @@ module EightyeightpagesClient
       self.class.base_uri "#{account_name}.88pages.com"
     end
 
-    def unescape(hash)
-      if hash.is_a?(Array)
-        hash.each do |value|
-          unescape hash
-        end
-      elsif hash.is_a?(Hash)
-        hash.keys.each do |key|
-          if hash[key].is_a?(String)
-            hash[key] = CGI::unescape(hash[key])
-          elsif hash[key].is_a?(Hash) or hash[key].is_a?(Array)
-            unescape hash[key]
-          end
-        end
-      end
-      hash
-    end
-
     def method_missing(form, *args)
       begin
-        unescape self.class.get("/api/forms/#{form.to_s.gsub('_', '-')}/entries.json?referer=#{referer}").parsed_response
+        Map.new self.class.get("/api/forms/#{form.to_s.gsub('_', '-')}/entries.json?referer=#{referer}").parsed_response
       rescue
         rescue super(field, *args)
       end
