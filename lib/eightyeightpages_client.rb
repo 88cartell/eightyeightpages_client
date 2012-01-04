532c1327ce3a59b344d8cf57620d64af9482b0e7
Carl Woodward <cjwoodward@gmail.com>
2012-01-04 16:08:47 +1100

    Turns out unescaping wasn't required

--- lib/eightyeightpages_client.rb
+++ lib/eightyeightpages_client.rb
@@ -12,24 +12,9 @@ module EightyeightpagesClient
       self.class.base_uri "#{account_name}.88pages.com"
     end
 
-    def unescape(value)
-      case value
-      when Array
-        value.map { |v| unescape(v) }
-      when Hash
-        Hash[value.map { |k, v| [k, unescape(v)] }]
-      else
-        if value.is_a?(String)
-          CGI::unescape(value)
-        else
-          value
-        end
-      end
-    end
-
     def method_missing(form, *args)
       begin
-        unescape self.class.get("/api/forms/#{form.to_s.gsub('_', '-')}/entries.json?referer=#{referer}").parsed_response
+        self.class.get("/api/forms/#{form.to_s.gsub('_', '-')}/entries.json?referer=#{referer}").parsed_response
       rescue
         rescue super(field, *args)
       end
